0acfc95648673ffe050bc084614ed76f9eb12975
Antonio Brasilio <antonioalves05555@gmail.com>
2024-12-30 12:02:23 -0300

    feat: Working with inputs and buttons

--- Project1-expo/App.js
+++ Project1-expo/App.js
@@ -1,5 +1,5 @@
 import { Component } from 'react';
-import { Button, Image, Text, View } from 'react-native';
+import { Button, Image, StyleSheet, Text, TextInput, View } from 'react-native';
 
 class App extends Component {
     constructor(props) {
@@ -10,30 +10,32 @@ class App extends Component {
         };
 
         this.login = this.login.bind(this);
+        this.handleChangeText = this.handleChangeText.bind(this);
     }
 
     login(name) {
         this.setState({ name });
     }
 
+    handleChangeText(event) {
+        this.setState({ name: event });
+    }
+
     render() {
         const img = 'https://sujeitoprogramador.com/steve.png';
 
         return (
-            <View style={{ marginTop: 50 }}>
+            <View style={[{ marginTop: 50 }, styles.container]}>
+                <TextInput
+                    onChangeText={this.handleChangeText}
+                    style={{ height: 40, borderColor: 'gray', borderWidth: 1, width: 200, margin: 15 }}
+                />
                 <Button
                     style={{ margin: 15 }}
                     title="Login"
                     onPress={() => this.login('Antonio')}
                 />
                 <Text style={{ color: '#FF0000', fontSize: 25, margin: 15 }}>{this.state.name}</Text>
-
-                <Jobs
-                    width={100}
-                    height={100}
-                    name="Steve Jobs"
-                    img={img}
-                />
             </View>
         );
     }
@@ -54,6 +56,15 @@ class Jobs extends Component {
         );
     }
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#FFF',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+});
 
 export default App;
 
