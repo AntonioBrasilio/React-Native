9e946a03584f41edf6dfdeaee904d40d72a94179
Antonio Brasilio <antonioalves05555@gmail.com>
2024-12-29 21:02:12 -0300

    feat: Understanding props and state

--- Project1-expo/App.js
+++ Project1-expo/App.js
@@ -1,20 +1,59 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Component } from 'react';
+import { Button, Image, Text, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+class App extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            name: '',
+        };
+
+        this.login = this.login.bind(this);
+    }
+
+    login(name) {
+        this.setState({ name });
+    }
+
+    render() {
+        const img = 'https://sujeitoprogramador.com/steve.png';
+
+        return (
+            <View style={{ marginTop: 50 }}>
+                <Button
+                    style={{ margin: 15 }}
+                    title="Login"
+                    onPress={() => this.login('Antonio')}
+                />
+                <Text style={{ color: '#FF0000', fontSize: 25, margin: 15 }}>{this.state.name}</Text>
+
+                <Jobs
+                    width={100}
+                    height={100}
+                    name="Steve Jobs"
+                    img={img}
+                />
+            </View>
+        );
+    }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+class Jobs extends Component {
+    render() {
+        const { width, height, name, img } = this.props;
+
+        return (
+            <View>
+                <Image
+                    source={{ uri: img }}
+                    style={{ width, height }}
+                />
+                <Text>{name}</Text>
+            </View>
+        );
+    }
+}
+
+export default App;
+
