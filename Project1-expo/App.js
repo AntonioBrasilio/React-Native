8093f9b9351840d1aea02975be46ca24cebd8864
Antonio Brasilio <antonioalves05555@gmail.com>
2024-12-31 14:32:34 -0300

    feat: App Lucky Cookie

--- Project1-expo/App.js
+++ Project1-expo/App.js
@@ -1,70 +1,59 @@
 import { Component } from 'react';
-import { Button, Image, StyleSheet, Text, TextInput, View } from 'react-native';
+import { Button, Image, Text, TouchableOpacity, View } from 'react-native';
 
 class App extends Component {
     constructor(props) {
         super(props);
-
         this.state = {
-            name: '',
+            selectedPhrase: '',
+            image: require('./assets/biscoito.png'),
         };
-
-        this.login = this.login.bind(this);
-        this.handleChangeText = this.handleChangeText.bind(this);
-    }
-
-    login(name) {
-        this.setState({ name });
-    }
-
-    handleChangeText(event) {
-        this.setState({ name: event });
     }
 
     render() {
-        const img = 'https://sujeitoprogramador.com/steve.png';
+        const phrases = [
+            'Siga os bons e aprenda com eles.',
+            'O bom-senso vale mais do que muito conhecimento.',
+            'O riso é a menor distância entre duas pessoas.',
+            'Deixe de lado as preocupações e seja feliz.',
+            'Realize o óbvio, pense no improvável e conquiste o impossível.',
+            'Acredite em milagres, mas não dependa deles.',
+            'A maior barreira para o sucesso é o medo do fracasso.',
+        ];
 
         return (
-            <View style={[{ marginTop: 50 }, styles.container]}>
-                <TextInput
-                    onChangeText={this.handleChangeText}
-                    style={{ height: 40, borderColor: 'gray', borderWidth: 1, width: 200, margin: 15 }}
-                />
-                <Button
-                    style={{ margin: 15 }}
-                    title="Login"
-                    onPress={() => this.login('Antonio')}
-                />
-                <Text style={{ color: '#FF0000', fontSize: 25, margin: 15 }}>{this.state.name}</Text>
-            </View>
-        );
-    }
-}
-
-class Jobs extends Component {
-    render() {
-        const { width, height, name, img } = this.props;
-
-        return (
-            <View>
+            <View style={styles.container}>
                 <Image
-                    source={{ uri: img }}
-                    style={{ width, height }}
+                    source={this.state.image}
+                    style={{ width: 250, height: 250 }}
                 />
-                <Text>{name}</Text>
+
+                <Text style={{ marginVertical: 10 }}>{this.state.selectedPhrase}</Text>
+
+                <TouchableOpacity
+                    style={{ backgroundColor: 'lightblue', padding: 10, borderRadius: 5 }}
+                    onPress={() => {
+                        this.setState({
+                            selectedPhrase: phrases[Math.floor(Math.random() * phrases.length)],
+                            image: require('./assets/biscoitoAberto.png'),
+                        });
+                    }}>
+                    <View>
+                        <Text>Quebrar biscoito</Text>
+                    </View>
+                </TouchableOpacity>
             </View>
         );
     }
 }
 
-const styles = StyleSheet.create({
+const styles = {
     container: {
         flex: 1,
-        backgroundColor: '#FFF',
-        alignItems: 'center',
         justifyContent: 'center',
+        alignItems: 'center',
     },
-});
+};
 
 export default App;
 
